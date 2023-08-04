9b6f2eb415b4c6d1fb5c73ba55f5b3dde9b3f7ff
cod2048 <cod2048@gmail.com>
2023-08-04 13:06:27 +0900

    스크립트 여닫는 버튼 추가, 발표 종료시 참관자에게 알림 보냄

--- src/pages/Practice.js
+++ src/pages/Practice.js
@@ -128,34 +128,34 @@ const Practice = () => {
 
   useEffect(() => {
     let intervalId;
-  
+
     if (listening) {
       intervalId = setInterval(() => {
         if (pauseStartTimeRef.current) {
           const pauseEndTime = Date.now();
           const pauseDuration = pauseEndTime - pauseStartTimeRef.current;
           console.log("무음 지속 시간 (밀리초):", pauseDuration);
-  
+
           if (compareMessage.current !== "긴장 풀고 다시 발표 내용을 떠올려보세요!" && compareMessage.current !== `표정이 너무 굳어있네요! SMILE*^-^*` && pauseDuration > 5000) {
             compareMessage.current = "긴장 풀고 다시 발표 내용을 떠올려보세요!";
             setMessage("긴장 풀고 다시 발표 내용을 떠올려보세요!");
             console.log("렌더링됨");
             pauseStartTimeRef.current = null; // 무음 시작 시간 초기화
           }
-          else if(pauseDuration < 5000){
+          else if (pauseDuration < 5000) {
             compareMessage.current = '좋습니다! 자신감을 갖고 계속 진행하세요!';
             setMessage('좋습니다! 자신감을 갖고 계속 진행하세요!');
           }
         }
       }, 1000);
     }
-  
+
     return () => {
       clearInterval(intervalId);
       pauseStartTimeRef.current = null; // 컴포넌트가 언마운트되면 무음 시작 시간 초기화
     };
-  }, [listening]); 
-  
+  }, [listening]);
+
   const handleStartStopListening = () => {
     if (!recognitionRef.current) {
       const isSpeechRecognitionSupported =
@@ -853,6 +853,12 @@ const Practice = () => {
     navigator.clipboard.writeText(roomName2);
   }
 
+  //스크립트 숨기기 기능 추가
+  const [showScript, setShowScript] = useState(true);
+  const handleToggleScript = () => {
+    setShowScript((prevShowScript) => !prevShowScript); // 스크립트 보이기/숨기기 상태를 반전시킴
+  };
+
 
   return (
     <div className="practice-container">
@@ -921,7 +927,7 @@ const Practice = () => {
         <div className='keyword-container'>
           <h1 className='keyword-title'>키워드 등록</h1>
           <div className='yes-word-container'>
-            권장단어
+            강조단어
             <input
               className="yes-word-input"
               type="text"
@@ -995,6 +1001,12 @@ const Practice = () => {
                   <GrLinkNext size={24} />
                 </button>
               )}
+              {playing ? (
+                <button onClick={handleToggleScript} className="script-show-button">
+                  {showScript ? "스크립트 숨기기" : "스크립트 보기"}
+                </button>
+              ) : null}
+
             </div>
             {!playing && (
               <textarea
@@ -1006,11 +1018,12 @@ const Practice = () => {
             )}
             {playing && (
               <div>
-                <div>
-                  {scriptArray[currentScriptIndex].split("\n").map((line, lineIndex) => (
-                    <div key={lineIndex} className="script-save">{line}</div>
+                {showScript &&
+                  scriptArray[currentScriptIndex].split("\n").map((line, lineIndex) => (
+                    <div key={lineIndex} className="script-save">
+                      {line}
+                    </div>
                   ))}
-                </div>
               </div>
             )}
           </div>
@@ -1085,11 +1098,6 @@ const Practice = () => {
                 className="real-live-camera"
                 muted
               ></video>
-              {/* <ul className="page-time-array">
-                {pageTimeArray.map((time, index) => (
-                  <li>페이지 {index + 1} : <span className="">{time.minutes} : {time.seconds} </span></li>
-                ))}
-              </ul> */}
               {playing ? (
                 <p className="real-title-save">{title}</p>
               ) : (
@@ -1101,7 +1109,6 @@ const Practice = () => {
                   onChange={(e) => titleChange(e)}
                 />
               )}
-              {/* <div className="message">{message}</div> */}
               <br />
               {playing ? (
                 <button onClick={quitPractice} className="practice-stop-button">
@@ -1124,7 +1131,7 @@ const Practice = () => {
             <div className="modal-keyword-container modal-result-summary">
               <h1>등록된 키워드</h1>
               <div className="modal-recommend-word-container">
-                <h2 className="modal-recommend-word-title">권장 단어</h2>
+                <h2 className="modal-recommend-word-title">강조 단어</h2>
                 <div className="modal-recommend-word">
                   {recommendedWords.map((word, index) => (
                     <div>
@@ -1153,7 +1160,7 @@ const Practice = () => {
             </div>
           </div>
           <div className="modal-middle">
-            <img src={logo} className="modal-logo" alt="logo" width={200} />
+            <img src={logo} className="modal-logo" alt="logo" width={250} />
             <h2 className="modal-title">{title}</h2>
             <video
               className="modal-video"
